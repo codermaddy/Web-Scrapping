4d3e347831a1c733c0006e41034991f9a48658e0
codermaddy <hiindia.himanshu@gmail.com>
2020-03-25 14:49:55 +0530

    entire project

---
+++ scrapper_test.py
@@ -0,0 +1,63 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from newspaper import Article
+import nltk
+import csv
+
+nltk.download('punkt')
+
+main_news_file = open('main_news.csv', 'w')
+sub_news_file = open('sub_news.csv', 'w')
+
+field_names = ['Title', 'URL', 'Datetime', 'Summary']
+
+main_news_writer = csv.DictWriter(main_news_file, field_names)
+sub_news_writer = csv.DictWriter(sub_news_file, field_names)
+
+url = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pKVGlnQVAB?hl=en-IN&gl=IN&ceid=IN%3Aen'
+leading_news_url = 'https://news.google.com'
+
+connection = urlopen(url)
+
+response = connection.read().decode('utf-8') 
+
+soup = BeautifulSoup(response, 'html.parser')
+
+section = soup.find('div', {'class' : 'lBwEZb BL5WZb GndZbb'})
+
+main_news_writer.writeheader()
+sub_news_writer.writeheader()
+
+for news in list(section.children)[:10]:
+	headings = news.find_all('a', {'class': 'DY5T1d'})
+	
+	main_news_heading = headings[0].string
+	main_news_url = leading_news_url + headings[0]['href'][1:]
+	
+	try:
+		article = Article(main_news_url)
+		article.download()
+		article.parse()
+		article.nlp()
+		main_news_writer.writerow({'Title': main_news_heading, 'URL': main_news_url, 'Datetime': article.publish_date, 'Summary': article.summary})
+
+	except:
+		pass
+	for sub_news in headings[1:]:
+		sub_news_heading = sub_news.string
+		sub_news_url = leading_news_url + sub_news['href'][1:]
+
+		try:
+			article = Article(sub_news_url)
+			article.download()
+			article.parse()
+			article.nlp()
+
+			sub_news_writer.writerow({'Title': sub_news_heading, 'URL': sub_news_url, 'Datetime': article.publish_date, 'Summary': article.summary})
+		
+		except:
+			pass
+
+main_news_file.close()
+sub_news_file.close()
+connection.close() 
---
+++ stock_data.py
@@ -0,0 +1,81 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from sched import scheduler
+from time import time, sleep
+from csv import DictWriter
+
+url = 'https://www.moneycontrol.com/stocks/marketstats/indexcomp.php?optex=NSE&opttopic=indexcomp&index=9'
+
+sc = scheduler(time, sleep)
+
+field_names = ['Company Name', 'Industry', 'Last Price', 'Change', '%Chg', 'Mkt Cap']
+
+PERC_CHNG_IDX = 4
+COMP_NAME_IDX = 0
+CHNG_MAGNITUDE = 2.00
+CHECK_INTERVAL = 4  	#4*30sec = 2 mins
+
+data = []
+
+counter = 0
+
+def check_change(data_recent):
+	if len(data):
+		print("CHECKING FOR CHANGE!")
+		for i in range(len(data)):
+			if abs(float(data_recent[i][PERC_CHNG_IDX]) - float(data[i][PERC_CHNG_IDX])) >= CHNG_MAGNITUDE:
+				print("ATTENTION!", end = ' ')
+				print("Change of %f in %s" %(float(data_recent[i][PERC_CHNG_IDX]) - float(data[i][PERC_CHNG_IDX]), data_recent[i][COMP_NAME_IDX]))
+		print("-."*20) 
+
+def read_data():
+
+	global counter
+	global data
+
+	connection = urlopen(url)
+
+	response = connection.read()
+	response = response.decode('utf-8')
+
+	soup = BeautifulSoup(response, 'html.parser')
+
+	section = soup.find('table', {'class': 'tbldata14 bdrtpg'})
+
+	#print(time())
+
+	data_recent = []
+
+	for row in section.find_all('tr')[1:]:
+
+		row_content = []
+
+		for idx, column in enumerate(row.find_all('td', {'class': 'brdrgtgry'})):
+
+			if idx < 2:
+				tmp = column.find('a').string
+			else:
+				tmp = column.string
+
+			row_content.append(tmp)
+
+		data_recent.append(row_content)
+
+	connection.close()
+
+	#print(counter)
+
+	if not len(data):
+		for row in data_recent:
+			print(row)
+
+	if counter == 0:
+		check_change(data_recent)
+		data = data_recent
+	
+	counter = (counter+1)%CHECK_INTERVAL
+	
+	sc.enter(30, 1, read_data)
+
+sc.enter(0, 1, read_data)
+sc.run()
